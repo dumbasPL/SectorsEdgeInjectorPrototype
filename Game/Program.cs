ae7d33d2b8515f1e0b893b8c00f0c505f94c1da6
dumbasPL <29180158+dumbasPL@users.noreply.github.com>
2020-08-31 15:18:28 +0200

    Initial commit

---
+++ Game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            while (true)
+            {
+                Console.WriteLine("Hello! " + DateTime.Now.ToString());
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Hack/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack
+{
+    public class Evil
+    {
+
+        public static void Main()
+        {
+            ConsoleColor cc = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Injected!");
+            Console.ForegroundColor = cc;
+        }
+
+    }
+}
